d9b84a970972b259484b8166904c31393525047d
Pushkar Dangi <pushkardangi746@gmail.com>
2024-12-21 10:58:01 +0530

    feat: establish MongoDB connection

---
+++ src/db/index.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js"
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+
+        console.log(`MongoDB connected successfully!! DB Host: ${connectionInstance.connection.host}`)
+        // console.log(connectionInstance)
+
+    } catch (error) {
+        console.error("Error while connecting DB:", error);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
