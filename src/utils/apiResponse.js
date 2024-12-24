d2d08c55dd732cf7e32c8847875b0b20ee9bb529
Pushkar Dangi <pushkardangi746@gmail.com>
2024-12-24 22:12:37 +0530

    refactor

--- src/utils/apiResponse.js
+++ src/utils/apiResponse.js
@@ -4,10 +4,10 @@ class apiResponse {
         data,
         message="Success"
     ) {
-        this.statusCode = statusCode
-        this.data = data
-        this.message = message
-        this.success = statusCode < 400
+        this.statusCode = statusCode;
+        this.data = data;
+        this.message = message;
+        this.success = statusCode < 400;
     }
 }
 
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,12 +1,13 @@
-const asyncHandler = (fn) => {
+const asyncHandler = (requestHandler) => {
     return async (req, res, next) => {
         try {
-            await fn(req, res, next);
+            await requestHandler(req, res, next);
         } catch (error) {
             res.status(error.code || 500).json({
                 success: false,
                 message: error.message,
-            })
+            });
+            next(error);
         }
     };
 };
