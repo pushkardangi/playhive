2bb2e3592f7411d5212bd84c443b6606bffd171f
Pushkar Dangi <pushkardangi746@gmail.com>
2025-02-11 17:21:12 +0530

    feat:added comment controller and routes boilerpalte

--- src/app.js
+++ src/app.js
@@ -19,11 +19,13 @@ app.use(cookieParser());
 import userRouter from "./routes/user.routes.js";
 import videoRouter from "./routes/video.routes.js";
 import tweetRouter from "./routes/tweet.routes.js";
+import commentRouter from "./routes/comment.routes.js";
 
 // declaring routes
 // http://localhost:3000/api/v1
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/videos", videoRouter);
 app.use("/api/v1/tweets", tweetRouter);
+app.use("/api/v1/comments", commentRouter);
 
 export default app;
